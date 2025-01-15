3512d376b56dce39467fb6ff630ed0095e40eb23
Gabriel DBA <gabialexandremedeiros@gmail.com>
2025-01-15 10:33:26 -0300

    exercicio-2

---
+++ january/week-2/wednesday/exercise_02_select_distinct_ratings.sql
@@ -0,0 +1,8 @@
+/*
+Exercise 2: Select all distinct ratings
+Difficulty: Basic
+Description: Write a query to select all distinct ratings.
+Expected Output: A result set containing all distinct ratings.
+*/
+
+select distinct rating from film;
