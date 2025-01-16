5ded1b48a74d768ee933dbacce79fca018327777
Gabriel DBA <gabialexandremedeiros@gmail.com>
2025-01-15 22:38:22 -0300

    Create exercise_19_movies_title_uppercase.sql

---
+++ january/week-2/wednesday/exercise_19_movies_title_uppercase.sql
@@ -0,0 +1,8 @@
+/*
+Exercise 19: Convert film titles to uppercase
+Difficulty: Basic
+Description: Write a query to convert film titles to uppercase.
+Expected Output: A result set containing convert film titles to uppercase.
+*/
+
+select upper(title) as title_uppercase from film;
