52474866d6f2da5767ace2438b1598a13e6ea1da
Gabriel DBA <gabialexandremedeiros@gmail.com>
2025-01-15 21:45:11 -0300

    Create exercise_12_highest_replacement_cost.sql

---
+++ january/week-2/wednesday/exercise_12_highest_replacement_cost.sql
@@ -0,0 +1,8 @@
+/*
+Exercise 12: Find the highest replacement cost
+Difficulty: Basic
+Description: Write a query to find the highest replacement cost.
+Expected Output: A result set containing the highest replacement cost.
+*/
+
+select max(replacement_cost) from film;
