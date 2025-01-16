bfd395506adb543829d0faa6d93ff4ed97f6ecf3
Gabriel DBA <gabialexandremedeiros@gmail.com>
2025-01-15 21:37:11 -0300

    Create exercise_11_average_rental_duration.sql

---
+++ january/week-2/wednesday/exercise_11_average_rental_duration.sql
@@ -0,0 +1,8 @@
+/*
+Exercise 11: Calculate average rental duration
+Difficulty: Basic
+Description: Write a query to calculate average rental duration.
+Expected Output: A result set containing average rental duration.
+*/
+
+select avg(rental_duration) from film;
